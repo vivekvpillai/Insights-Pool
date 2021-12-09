98d59421567bc68e523383574199e3210c90f843
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-12-08 19:04:17 -0600

    update react code not working

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,152 +1,143 @@
-class App extends React.Component {
-    state = {
-        objects:[]
-    }
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
-    componentDidMount = () => {
-        axios.get('/objects').then(
-            (response) => {
-                this.setState({
-                    objects:response.data
-                })
-                console.log(this.state.objects)
-            }
-        )
-    }
+function App() {
 
-    createobject = (event) => {
-        event.preventDefault();
-        axios.post(
-            '/objects',
+  const [objects, setObjects] = useState([]);
+  const [newsampleName, setsampleName] = useState('');
+  const [newcategory, setCategory] = useState('');
+  const [newvisits, setVisits] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('/objects').then((response)=>{
+        console.log(response.data);
+        setObjects(response.data)
+      })
+  }, [])
+
+
+
+  const createobject = (event) => {
+      event.preventDefault();
+      axios.post(
+          '/objects',
+          {
+              sampleName:newsampleName,
+              category:newcategory,
+              visits:newvisits,
+          }
+      ).then(() => {
+        axios
+            .get('/objects')
+            .then((response)=> {
+              setObjects(response.data)
+            })
+      })
+  }
+
+  const handleNewVisits = (event) => {
+      setVisits(event.target.value)
+  }
+
+  const handleNewCategory = (event) => {
+      setCategory(event.target.value)
+  }
+
+  const handleNewSampleName = (event) => {
+      setsampleName(event.target.value)
+  }
+
+  const deleteObject = (event) => {
+      axios.delete('/objects/' + event.target.value).then(() =>{
+        axios
+          .get('/objects')
+          .then((response) => {
+            setObjects(response.data)
+        })
+      })
+  }
+
+  const updateObject = (event) => {
+      event.preventDefault();
+      const id = event.target.getAttribute('id');
+      axios.put(
+          '/objects/' + id,
+          {
+              sampleName:newsampleName,
+              category:newcategory,
+              visits:newvisits
+          }
+      ).then(()=>{
+          axios
+            .get('/objects/')
+            .then((response)=> {
+              setObjects(response.data)
+            })
+      })
+  }
+
+  changeupdateObjectsampleName = (event) => {
+      this.setState(
+          {
+              updateObjectsampleName:event.target.value
+          }
+      )
+  }
+
+  changeupdateObjectcategory = (event) => {
+      this.setState(
+          {
+              updateObjectcategory:event.target.value
+          }
+      )
+  }
+
+  changeupdateObjectvisits = (event) => {
+      this.setState(
+          {
+              updateObjectvisits:event.target.value
+          }
+      )
+  }
+
+  return(
+    <div>
+        <h2>Create object</h2>
+        <form onSubmit={createobject}>
+            <input onChange={handleNewSampleName} type="text" placeholder="sampleName" /><br/>
+            <input onChange={handleNewCategory} type="text" placeholder="category" /><br/>
+            <input onChange={handleNewVisits} type="number" placeholder="visits" /><br/>
+            <input type="submit" value="Create object" />
+        </form>
+        <h2>List of objects</h2>
+        <ul>
             {
-                sampleName:this.state.newobjectsampleName,
-                category:this.state.newobjectcategory,
-                visits:this.state.newobjectvisits,
+                objects
+                .sort(({ id: previousID }, { id: currentID }) => previousID - currentID)
+                .map(
+                    (object, index) => {
+                      console.log(object);
+                      console.log(index)
+                        return <li key={index}>
+
+                            {object.sampleName}: {object.category} : {object.visits}
+
+                            <button value={object.id} onClick={deleteObject}>DELETE</button>
+
+                            <form id={object.id} onSubmit={updateObject}>
+                                <input onChange={handleNewSampleName} type="text" placeholder="sampleName"/><br/>
+                                <input onChange={handleNewCategory} type="text" placeholder="category"/><br/>
+                                <input onChange={handleNewVisits} type="number" placeholder="visits" /><br/>
+                                <input type="submit"  value="Update object"/>
+                            </form>
+                        </li>
+                    }
+                )
             }
-        ).then(
-            (response) => {
-                this.setState({
-                    objects:response.data
-                })
-            }
-        )
-    }
-
-    changeNewobjectvisits = (event) => {
-        this.setState({
-            newobjectvisits:event.target.value
-        });
-    }
-
-    changeNewobjectcategory = (event) => {
-        this.setState({
-            newobjectcategory:event.target.value
-        });
-    }
-
-    changeNewobjectsampleName = (event) => {
-        this.setState({
-            newobjectsampleName:event.target.value
-        });
-    }
-
-    deleteObject = (event) => {
-        axios.delete('/objects/' + event.target.value).then(
-            (response) => {
-                this.setState({
-                    objects:response.data
-                })
-            }
-        )
-
-    }
-
-    updateObject = (event) => {
-        event.preventDefault();
-        const id = event.target.getAttribute('id');
-        axios.put(
-            '/objects/' + id,
-            {
-                sampleName:this.state.updateObjectsampleName,
-                category:this.state.updateObjectcategory,
-                visits:this.state.updateObjectvisits
-            }
-        ).then(
-            (response) => {
-                this.setState({
-                    objects:response.data,
-                    sampleName:'',
-                    category:'',
-                    visits: null
-                })
-            }
-        )
-    }
-
-    changeupdateObjectsampleName = (event) => {
-        this.setState(
-            {
-                updateObjectsampleName:event.target.value
-            }
-        )
-    }
-
-    changeupdateObjectcategory = (event) => {
-        this.setState(
-            {
-                updateObjectcategory:event.target.value
-            }
-        )
-    }
-
-    changeupdateObjectvisits = (event) => {
-        this.setState(
-            {
-                updateObjectvisits:event.target.value
-            }
-        )
-    }
-
-    render = () => {
-        return <div>
-            <h2>Create object</h2>
-            <form onSubmit={this.createobject}>
-                <input onKeyUp={this.changeNewobjectsampleName} type="text" placeholder="sampleName" /><br/>
-                <input onKeyUp={this.changeNewobjectcategory} type="text" placeholder="category" /><br/>
-                <input onKeyUp={this.changeNewobjectvisits} type="number" placeholder="visits" /><br/>
-                <input type="submit" value="Create object" />
-            </form>
-            <h2>List of objects</h2>
-            <ul>
-                {
-                    this.state.objects
-                    .sort(({ id: previousID }, { id: currentID }) => previousID - currentID)
-                    .map(
-                        (object, index) => {
-                          console.log(object);
-                          console.log(index)
-                            return <li key={index}>
-
-                                {object.sampleName}: {object.category} : {object.visits}
-
-                                <button value={object.id} onClick={this.deleteObject}>DELETE</button>
-
-                                <form id={object.id} onSubmit={this.updateObject}>
-                                    <input onKeyUp={this.changeupdateObjectsampleName} type="text" placeholder="sampleName"/><br/>
-                                    <input onKeyUp={this.changeupdateObjectcategory} type="text" placeholder="category"/><br/>
-                                    <input onKeyUp={this.changeupdateObjectvisits} type="number" placeholder="visits" /><br/>
-                                    <input type="submit"  value="Update object"/>
-                                </form>
-                            </li>
-                        }
-                    )
-                }
-            </ul>
-        </div>
-    }
+        </ul>
+    </div>
+  )
 }
 
-ReactDOM.render(
-    <App></App>,
-    document.querySelector('main')
-)
+export default App;
