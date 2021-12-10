7b96a2ea10c5fb666f2ce30dcb7a4cba6ad01cd2
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-12-09 20:24:18 -0600

    crossorigin CORS

--- src/main/java/com/Vivek/Insights/InsightsApplication.java
+++ src/main/java/com/Vivek/Insights/InsightsApplication.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
+@CrossOrigin
 @RestController
 public class InsightsApplication {
 	@Autowired
